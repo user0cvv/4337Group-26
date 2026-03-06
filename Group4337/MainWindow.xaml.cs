7bd16627665840018dab918cb69bef004c892626
arnil <arnil@sakaev.com>
2026-03-06 16:37:13 +0300

    Added author window and button

--- Group4337/MainWindow.xaml.cs
+++ Group4337/MainWindow.xaml.cs
@@ -7,4 +7,9 @@ namespace Group4337
         public MainWindow()
             => InitializeComponent();
     }
-}
+    private void Author_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+    }
